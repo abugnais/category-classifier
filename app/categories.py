306f82258ddd85e25cf85e69c5f1f3a27048a81d
Ahmad Moqanasa <ahmax.moqanasa@gmail.com>
2025-06-20 23:35:51 +0100

    add food item

--- app/categories.py
+++ app/categories.py
@@ -3,5 +3,6 @@ CATEGORIES = {
     "clothes": "This is a clothing item",
     "electronics": "This is an electronic product",
     "books": "This is a written publication",
-    "furniture": "This is a piece of furniture"
+    "furniture": "This is a piece of furniture",
+    "Food": "This is yummy"
 }
